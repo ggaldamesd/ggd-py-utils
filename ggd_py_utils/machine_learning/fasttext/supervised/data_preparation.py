33793ee293bceb9fa831e756229b6920f3de4313
Gerardo Galdames <gerardo.galdames@iconstruye.com>
2024-08-30 00:31:01 -0400

    .

--- ggd_py_utils/machine_learning/fasttext/supervised/data_preparation.py
+++ ggd_py_utils/machine_learning/fasttext/supervised/data_preparation.py
@@ -2,7 +2,10 @@ from pandas import DataFrame, Series, to_numeric, concat
 from tqdm.notebook import tqdm
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import train_test_split
+from ggd_py_utils.formating.numeric import abbreviate_large_number
 from ggd_py_utils.machine_learning.data.cleaning import clean_text
+from ggd_py_utils.machine_learning.data.corpus_metrics import get_words_and_subwords_counts
+from ggd_py_utils.tracing.metrics import time_block
 
 tqdm.pandas()
 
@@ -94,3 +97,68 @@ def format_fasttext(df:DataFrame, feature_name:str="Features", label_name:str="L
         df.to_csv(path_or_buf=path, index=False, header=False, sep="\t")
 
     return df
+
+def prepare_corpus_dataframe(df:DataFrame, fields_to_clean:list, label_code:str, label_name:str, features_fields:list, corpus_ft_path:str, validation_corpus_ft_path:str, dimensions:int=300):
+    print(f"Initial Dataframe shape: {df.shape}")
+    
+    with time_block(block_name="clean_dataframe"):
+        df:DataFrame = clean_dataframe(df=df, fields=fields_to_clean, inplace=True)
+        print(f"Dataframe shape after first clean: {df.shape}")
+
+    with time_block(block_name="clean_dataframe"):
+        df:DataFrame = text_label_to_numeric(df=df, label_code_name=label_code)
+
+    with time_block(block_name="label_concatenation"):
+        df:DataFrame = label_concatenation(df=df, label_fields=[label_code, label_name])
+
+    with time_block(block_name="features_concatenation"):
+        df:DataFrame = features_concatenation(df=df, features_fields=features_fields)
+
+    with time_block(block_name="clean_features"):
+        df:DataFrame = clean_features(df=df)
+
+    with time_block(block_name="clean_features"):
+        df:DataFrame = drop_invalid_data(df=df, label_code_name=label_code)
+        print(f"Dataframe shape after fatures clean: {df.shape}")
+
+    with time_block(block_name="drop_repeated_features"):
+        df:DataFrame = drop_repeated_features(df=df)
+        print(f"Dataframe shape after drop_repeated_features: {df.shape}")
+
+    with time_block(block_name="drop_invalid_features_data"):
+        df:DataFrame = drop_invalid_features_data(df=df)
+        print(f"Dataframe shape after drop_invalid_features_data: {df.shape}")
+        
+    with time_block(block_name="drop_numeric_encoded_labels"):
+        df:DataFrame = drop_numeric_encoded_labels(df=df)
+        print(f"Dataframe shape after drop_numeric_encoded_labels: {df.shape}")
+
+    with time_block(block_name="get_minimal_corpus_dataframe"):
+        df:DataFrame = get_minimal_corpus_dataframe(df=df)
+
+    with time_block(block_name="balance_training_data"):
+        df:DataFrame = balance_training_data(df=df)
+        print(f"Dataframe shape after balance_training_data: {df.shape}")
+
+    with time_block(block_name="split_train_test_data"):
+        train, test = split_train_test_data(df=df)
+
+        print("Training Set:", len(train))
+        print("Test Set:", len(test))
+
+    with time_block(block_name="format_fasttext_train_data"):
+        format_fasttext(df=train, path=corpus_ft_path)
+        
+    with time_block(block_name="format_fasttext_validation_data"):
+        format_fasttext(df=test, path=validation_corpus_ft_path)
+
+    words_and_subwords_counts:dict = get_words_and_subwords_counts(filename=corpus_ft_path)
+
+    words:int = words_and_subwords_counts["words"]
+    subwords:int = words_and_subwords_counts["subwords"]
+    tokens:int = words + subwords
+
+    estimated_params:int = dimensions * tokens
+    estimated_params_formated:str = abbreviate_large_number(number=estimated_params)
+
+    print(f"Estimated corpus parameters: {estimated_params} / {estimated_params_formated}")
