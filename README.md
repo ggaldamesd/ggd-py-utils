74047ca984f894dfc45f2eafa8838235ddc44ebd
Gerardo Galdames <gerardo.galdames@iconstruye.com>
2024-08-30 11:29:56 -0400

    .

--- README.md
+++ README.md
@@ -1,52 +1,34 @@
-# ggd_py_utils
+**Package Purpose**
 
-Un conjunto de utilidades para proyectos de Python.
+The `ggd-py-utils` package is a collection of reusable utilities for Python projects. The package is designed to provide a set of useful tools for various tasks, including tracing, machine learning, and formatting.
 
-## Propósito del Paquete
+**Package Structure**
 
-El objetivo de este paquete es proveer una serie de utilidades reutilizables para proyectos de Python. Estas utilidades se organizan en diferentes módulos y directorios, cada uno con su propósito específico.
+The package is organized into the following directories and modules:
 
-## Contenido del Paquete
+* `machine_learning/`: utilities for machine learning
+	+ `fasttext/`: utilities for FastText
+		- `supervised/`: utilities for supervised classification
+			- `training.py`: script for training models
+			- `data_preparation.py`: script for preparing data
+* `tracing/`: utilities for tracing
+	+ `metrics.py`: script for measuring execution time
+* `formating/`: utilities for formatting numbers and text
+	+ `numeric.py`: script for formatting numbers
 
-El paquete `ggd_py_utils` incluye las siguientes utilidades:
+**Package Contents**
 
-### Tracing
+The package includes the following files and directories:
 
-Las utilidades de tracing se utilizan para medir el tiempo de ejecución de bloques de código. Esto es útil para identificar cuellos de botella en el rendimiento de tu aplicación.
+* `ggd_py_utils/`: package directory
+	+ `machine_learning/`: machine learning utilities
+	+ `tracing/`: tracing utilities
+	+ `formating/`: formatting utilities
+	+ `README.md`: this README file
 
-### Machine Learning
+**Dependencies**
 
-Las utilidades de machine learning se utilizan para preparar datos y entrenar modelos de clasificación supervisada con FastText. Estas utilidades incluyen scripts para entrenar modelos y preparar datos para su uso en modelos de clasificación.
-
-### Formating
-
-Las utilidades de formating se utilizan para formatear números y texto. Estas utilidades incluyen funciones para formatear números de manera legible y para abreviar grandes números.
-
-## Estructura del Paquete
-
-El paquete se organiza de la siguiente manera:
-
-* `ggd_py_utils/`: directorio principal del paquete
-	+ `machine_learning/`: utilidades para machine learning
-		- `fasttext/`: utilidades para FastText
-			- `supervised/`: utilidades para clasificación supervisada
-				- `training.py`: script para entrenar modelos
-				- `data_preparation.py`: script para preparar datos
-	+ `tracing/`: utilidades para tracing
-		- `metrics.py`: script para medir el tiempo de ejecución
-	+ `formating/`: utilidades para formatear números y texto
-		- `numeric.py`: script para formatear números
-	+ `README.md`: este archivo de README
-
-## Instalación
-
-Para instalar el paquete, puedes ejecutar el siguiente comando:
-```bash
-pip uninstall ggd-py-utils; pip install git+https://github.com/ggaldamesd/ggd-py-utils.git
-```
-## Requisitos
-
-El paquete requiere las siguientes dependencias:
+The package requires the following dependencies:
 
 * `unidecode==1.3.8`
 * `nltk==3.8.1`
@@ -54,3 +36,22 @@ El paquete requiere las siguientes dependencias:
 * `tqdm==4.66.2`
 * `imbalanced-learn==0.12.3`
 * `scikit-learn==1.5.0`
+
+**Installation**
+
+To install the package, run the following command:
+```bash
+pip uninstall ggd-py-utils; pip install git+https://github.com/ggaldamesd/ggd-py-utils.git
+```
+**Updating**
+
+To update the package, run the following command:
+```bash
+pip install --upgrade git+https://github.com/ggaldamesd/ggd-py-utils.git
+```
+**Uninstallation**
+
+To uninstall the package, run the following command:
+```bash
+pip uninstall ggd-py-utils
+```
--- ggd_py_utils/machine_learning/fasttext/supervised/training.py
+++ ggd_py_utils/machine_learning/fasttext/supervised/training.py
@@ -5,68 +5,183 @@ from multiprocessing import cpu_count
 from ggd_py_utils.tracing.file import get_file_size
 from ggd_py_utils.tracing.metrics import time_block
 
-def train_fasttext_model(
+def merge_hyperparameters(defaults:dict, overrides:dict={}) -> dict:
+    """
+    Merge two dictionaries of hyperparameters. If a key is present in both dictionaries, the value from the overrides dictionary will be used.
+
+    Parameters
+    ----------
+    defaults : dict
+        A dictionary of default hyperparameters.
+    overrides : dict
+        A dictionary of hyperparameters to override the defaults.
+
+    Returns
+    -------
+    dict
+        A dictionary of hyperparameters with the overrides applied.
+    """
+    hyperparameters:dict = {key: overrides.get(key, default) for key, default in defaults.items()}
+    
+    return hyperparameters
+
+def train_fasttext_model_with_autotune(
         train_corpus_path:str, 
         validation_corpus_path:str, 
         model_file:str, 
-        auto_tune_model:bool=False, 
         threads:int=cpu_count(),
         autotune_duration_in_minutes:int=5,
         autotune_model_size_in_mb:int=250,
 ) -> _FastText:
+    
+    
     """
-    Train a FastText supervised model.
+    Train a FastText supervised model using autotune. The model is trained using the given training corpus and validated using the given validation corpus. The model is then saved to the given file.
 
     Parameters
     ----------
     train_corpus_path : str
-        The path to the file containing the training corpus.
+        The path to the training corpus.
     validation_corpus_path : str
-        The path to the file containing the validation corpus.
+        The path to the validation corpus.
     model_file : str
-        The path to save the trained model.
-    auto_tune_model : bool, optional
-        If True, the hyperparameters of the model will be tuned using the autotune feature of the FastText library, by default False.
+        The path to save the model.
     threads : int, optional
-        The number of threads to use, by default the number of CPUs available.
+        The number of threads to use, by default the number of CPUs.
     autotune_duration_in_minutes : int, optional
-        The duration of the autotune process in minutes, by default 5 minutes.
+        The duration of the autotune in minutes, by default 5.
     autotune_model_size_in_mb : int, optional
-        The size of the model in megabytes, by default 250MB.
+        The size of the model in megabytes, by default 250.
 
     Returns
     -------
     _FastText
         The trained model.
+
+    Notes
+    -----
+    The model is trained using the following hyperparameters:
+
+    - `verbose`: 2
+    - `thread`: The number of threads given
+    - `autotuneValidationFile`: The validation file given
+    - `autotuneMetric`: 'f1'
+    - `autotunePredictions`: 1
+    - `autotuneDuration`: The duration given in seconds
+    - `autotuneModelSize`: The size given in bytes
     """
     model:_FastText
     
-    if auto_tune_model:
-        autotune_duration:int = 60 * autotune_duration_in_minutes
-        autotune_model_size:int = 1024 * 1024 * autotune_model_size_in_mb
+    autotune_duration:int = 60 * autotune_duration_in_minutes
+    autotune_model_size:int = 1024 * 1024 * autotune_model_size_in_mb
 
-        with time_block(block_name="Training with autotune"):
-            model:_FastText = train_supervised(
-                input=train_corpus_path,
-                verbose=3,
-                thread=threads,
-                autotuneValidationFile=validation_corpus_path,
-                autotuneMetric="f1",
-                autotunePredictions=1,
-                autotuneDuration=autotune_duration,
-                autotuneModelSize=autotune_model_size
-            )
-    else:
-        with time_block(block_name="Training without autotune"):
-            model:_FastText = train_supervised(
-                input=train_corpus_path,
-                verbose=3,
-                thread=threads
-            )
-            
-        if not model.is_quantized(): 
-            with time_block(block_name="Quantizing model"):
-                model.quantize()
+    with time_block(block_name="Training with autotune"):
+        model:_FastText = train_supervised(
+            input=train_corpus_path,
+            verbose=2,
+            thread=threads,
+            autotuneValidationFile=validation_corpus_path,
+            autotuneMetric="f1",
+            autotunePredictions=1,
+            autotuneDuration=autotune_duration,
+            autotuneModelSize=autotune_model_size
+        )
+    
+    with time_block(block_name="Getting model metrics"):
+        metrics = model.test(path=validation_corpus_path, threshold=0.0)
+
+    print(f"Model samples: {metrics[0]}")
+    print(f"Model precision: {metrics[1]}")
+    print(f"Model recall: {metrics[2]}")
+
+    model_dimensions = model.get_dimension()
+    print(f"Model dimensions: {model_dimensions}")
+    
+    model.save_model(model_file)
+
+    _, model_size = get_file_size(filename=model_file)
+
+    print(f"Model size: {model_size}")
+    
+    return model
+
+def train_fasttext_model_with_hyperparameter(
+        train_corpus_path:str, 
+        validation_corpus_path:str, 
+        model_file:str, 
+        threads:int=cpu_count(),
+        hyperparameters:dict={},
+) -> _FastText:
+    """
+    Train a FastText model using the given hyperparameters.
+
+    Parameters
+    ----------
+    train_corpus_path : str
+        The path to the training corpus file.
+    validation_corpus_path : str
+        The path to the validation corpus file.
+    model_file : str
+        The path to save the model file.
+    threads : int, optional
+        The number of threads to use, by default the number of available CPU cores.
+    hyperparameters : dict, optional
+        The hyperparameters to use, by default an empty dictionary.
+
+    Returns
+    -------
+    _FastText
+        The trained model.
+
+    Notes
+    -----
+    The model is trained using the following default hyperparameters:
+    
+    - `lr`: 0.05
+    - `dim`: 100
+    - `ws`: 5
+    - `epoch`: 5
+    - `minCount`: 5
+    - `minn`: 3
+    - `maxn`: 6
+    - `wordNgrams`: 1
+    - `loss`: "softmax"
+    - `bucket`: 2000000
+    - `lrUpdateRate`: 100
+    - `verbose`: 2
+    - `pretrainedVectors`: ""
+    """
+    model:_FastText
+    
+    supervised_default:dict = {
+        'model': "supervised",
+        'lr': 0.05,
+        'dim': 100,
+        'ws': 5,
+        'epoch': 5,
+        'minCount': 5,
+        'minn': 3,
+        'maxn': 6,
+        'wordNgrams': 1,
+        'loss': "softmax",
+        'bucket': 2000000,
+        'lrUpdateRate': 100,
+        'verbose': 2,
+        'pretrainedVectors': "",
+    }
+
+    final_hyperparameters:dict = merge_hyperparameters(supervised_default, hyperparameters)
+
+    with time_block(block_name="Training without autotune"):
+        model:_FastText = train_supervised(
+            input=train_corpus_path,
+            thread=threads,
+            **final_hyperparameters
+        )
+        
+    if not model.is_quantized(): 
+        with time_block(block_name="Quantizing model"):
+            model.quantize()
 
     with time_block(block_name="Getting model metrics"):
         metrics = model.test(path=validation_corpus_path, threshold=0.0)
