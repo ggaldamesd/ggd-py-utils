162837c03f868e0ce25317241238dd9e17024c81
Gerardo Galdames <gerardo.galdames@iconstruye.com>
2024-09-01 22:43:42 -0400

    .

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "scikit-learn==1.5.0",
         "colorama==0.4.6",
         "chime==0.7.0",
-        "numpy==1.26.4"
+        "numpy==1.26.4",
+        "fasttext-wheel==0.9.2",
     ],
 )
